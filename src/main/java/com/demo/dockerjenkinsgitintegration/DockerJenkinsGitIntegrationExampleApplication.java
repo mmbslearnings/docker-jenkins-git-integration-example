cb5fff7e2d063552c01f54d032914f3f634ab668
Manoj <mmbsharma.a5@gmail.com>
2022-02-20 17:20:09 +0530

    Changed to Spring Boot

---
+++ src/main/java/com/demo/dockerjenkinsgitintegration/DockerJenkinsGitIntegrationExampleApplication.java
@@ -0,0 +1,13 @@
+package com.demo.dockerjenkinsgitintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJenkinsGitIntegrationExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJenkinsGitIntegrationExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/dockerjenkinsgitintegration/DockerJenkinsGitIntegrationExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.dockerjenkinsgitintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinsGitIntegrationExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
